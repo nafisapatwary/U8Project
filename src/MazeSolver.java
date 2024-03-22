61c2279461d7f447b5c75a6ad2ab9e5086cc055c
nafisapatwary <email@email.com>
2024-03-21 22:04:28 -0400

    i tried

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,16 +1,31 @@
 import java.util.ArrayList;
 public class MazeSolver {
-    public ArrayList<String> pathCoordinates(String[][] maze){
-        ArrayList<String> coords = new ArrayList<String>();
+    private String[][] maze;
+    private ArrayList<String> coords = new ArrayList<String>();
+
+    public MazeSolver(String[][] maze){
+        this.maze = maze;
+    }
+
+    public ArrayList<String> pathCoordinates(){
         boolean found = false;
         int r = 0;
         int c = 0;
         while (!found){
+//            printMaze();
+//            System.out.println();
             if (c == maze[0].length - 1 && r == maze.length - 1) found = true;
             if (maze[r][c].equals(".")) {
-                coords.add(returnAsCoords(r, c));
-                //make it so there's no chance of going back to an already found spot
-                maze[r][c] = "#";
+                if (isDeadEnd(r, c)){
+                    resetVisited();
+                    r = 0;
+                    c = 0;
+                }
+                else{
+                    //mark as visited
+                    coords.add(returnAsCoords(r, c));
+                    maze[r][c] = "V";
+                }
                 boolean moved = false;
                 //needs to check all directions
                 if (r != maze.length - 1) {
@@ -38,6 +53,8 @@ public class MazeSolver {
                     }
                 }
             }
+            printMaze();
+            System.out.println();
         }
         return coords;
     }
@@ -45,4 +62,30 @@ public class MazeSolver {
     public String returnAsCoords(int x, int y){
         return ("(" + x +", " + y + ")");
     }
+
+    public boolean isDeadEnd(int r, int c){
+        //help theres too many conditions omg omg mr das hellllllllllllp
+    }
+
+    public void printMaze(){
+        for (String[] s : maze){
+            for (String x: s){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+
+    public void resetVisited() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (maze[i][j].equals("V")) {
+                    maze[i][j] = ".";
+                }
+            }
+        }
+        //reset coords list
+        coords = new ArrayList<String>();
+    }
+
 }
--- src/Tester.java
+++ src/Tester.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         String[][] test1 = getMaze("Data/Maze");
-        MazeSolver m = new MazeSolver();
-
         String[][] test2 = {{".", ".", "."},
                             {"#", "#", "."},
                             {"#", ".", "."},
                             {"#", ".", "#"},
                             {"#", ".", "."}};
 
-
+        MazeSolver m = new MazeSolver(test1);
         System.out.println(
                 "[(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), (4, 2), (4, 1), (4, 0), (5, 0), (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), (6, 10), (6, 11), (6, 12), (6, 13), (6, 14), (6, 15), (6, 16), (5, 16), (4, 16), (3, 16), (2, 16), (1, 16), (1, 17), (1, 18), (1, 19), (1, 20), (1, 21), (1, 22), (1, 23), (1, 24), (2, 24), (3, 24), (4, 24), (5, 24), (6, 24), (7, 24), (7, 23), (7, 22), (7, 21), (8, 21), (9, 21), (9, 22), (9, 23), (9, 24), (9, 25), (9, 26)]");
-        System.out.println(m.pathCoordinates(test1));
+        System.out.println(m.pathCoordinates());
 
     }
     public static String[][] getMaze(String fileName) {
